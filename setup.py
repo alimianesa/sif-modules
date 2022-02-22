d82a06e291bed50a070a0b61c9beb39a25dbf36c
Ali Salimian <ali@gmail.com>
2022-02-22 16:02:57 +0330

    create py setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='sif_base_module',
+    packages=['sif-base-module'],
+    version='1.0.0.0',  # Ideally should be same as your GitHub release tag varsion
+    description='SAM IOT Base Module',
+    author='babak nodoust',
+    author_email='alive2212@gmail.com',
+    url='github package source url',
+    download_url='download link you saved',
+    keywords=['sam-iot-framework', 'sam', 'sam-iot', 'sif'],
+    classifiers=[],
+)
